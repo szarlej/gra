58d32c84b7b0869b14493d213bba42c755184150
Jarllaxe <jacek.m.pajak@gmail.com>
2012-11-07 17:19:21 +0100

    Bakap tego co mam

---
+++ src/com/enzo/cools/GraActivity.java
@@ -0,0 +1,13 @@
+package com.enzo.cools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GraActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
